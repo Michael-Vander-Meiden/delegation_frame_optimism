c4d0d6ca1b0081b649aa49863031e6e9c399436a
Michael Vander Meiden <michaelvandermeiden@gmail.com>
2024-08-09 18:28:57 -0600

    added stuff to get delegate from op token contract, still under dev

--- test_scripts/useful_api_functions.py
+++ test_scripts/useful_api_functions.py
@@ -1,4 +1,26 @@
 import requests
+import json
+from web3 import Web3
+
+def check_op_token_balance(addresses, infura_url):
+    # Replace with the OP token contract address on Optimism
+    OP_TOKEN_ADDRESS = '0x4200000000000000000000000000000000000042'
+    
+    # ERC-20 token standard ABI
+    OP_token_abi = 
+        # Connect to the Optimism network
+    w3 = Web3(Web3.HTTPProvider(infura_url))
+    
+    # Load the OP token contract
+    op_token_contract = w3.eth.contract(address=OP_TOKEN_ADDRESS, abi=OP_token_abi)
+    
+    balances = {}
+    
+    for address in addresses:
+        import pdb; pdb.set_trace()
+        print(op_token_contract.functions.delegates({address}).call())
+    
+    return balances
 
 def get_farcaster_following_fids(fid=192336):
     url = "https://api.neynar.com/v2/farcaster/following"
@@ -46,9 +68,14 @@ def _extract_eth_addresses(response_data):
     return eth_addresses
 
 # Example usage for getting following
-following_data = get_farcaster_following_fids()
-print(following_data)
+#following_data = get_farcaster_following_fids()
+#print(following_data)
 
-print("VERIFIED USER STUFF RETURNED BELOW:")
-ethereum_addresses = get_ethereum_addresses_from_fids(following_data)
-print(ethereum_addresses)
+#print("VERIFIED USER STUFF RETURNED BELOW:")
+#ethereum_addresses = get_ethereum_addresses_from_fids(following_data)
+#print(ethereum_addresses)
+
+infura_url = "https://optimism-mainnet.infura.io/v3/725e219fd58a4afcb8e2f4a3e5a40d91"
+test_addresses = ["0xa7162D7678dab477Be0ABc927a19cc4575d67Acd", "0x700e2cdeb777be3886ba58bd9aae619c53693844"]
+#op_balances = check_op_token_balance(test_addresses, infura_url)
+#print(op_balances)
