e106fe9004240d4ed8ef9d9b672ab8bc6976d25c
Michael Vander Meiden <michaelvandermeiden@gmail.com>
2024-08-10 09:35:29 -0600

    added lambda folder

--- lambda_functions/get_stats_lambda/lambda_function.py
+++ lambda_functions/get_stats_lambda/lambda_function.py
@@ -1,30 +1,21 @@
-
 import os
 import requests
 import json
-from web3 import Web3
 
-
-fid = "192336"
-
-
-# get verified ethereum address from farcaster ID
 def get_ethereum_addresses_from_fids(fids=[192336]):
     url = "https://api.neynar.com/v2/farcaster/user/bulk"
     
     headers = {
         "accept": "application/json",
-        "api_key": "NEYNAR_API_DOCS"
+        "api_key": os.environ['NEYANR_API_KEY']  # Store your API key in environment variables
     }
     
-    # Convert the list of FIDs to a comma-separated string for the params
     params = {
         "fids": ",".join(map(str, fids))
     }
     
     response = requests.get(url, headers=headers, params=params)
     
-    # Assuming the response is JSON and contains Ethereum addresses for each FID
     return _extract_eth_addresses(response.json())
 
 def _extract_eth_addresses(response_data):
@@ -33,15 +24,10 @@ def _extract_eth_addresses(response_data):
         addresses = user.get('verified_addresses', {}).get('eth_addresses', [])
         eth_addresses.extend(addresses)
     return eth_addresses
-#check delegate from ethereum address
 
-# check if delegate voted in most recent vote
 def get_stats_function(fid):
-
-    #Create dictionary that we will return
     return_package = {"hasVerifiedAddress": False, "hasDelegate": False, "isGoodDelegate": False, "delegateInfo": {}}
 
-    #check if there is a verified ethereum address
     eth_addresses = get_ethereum_addresses_from_fids([fid])
     
     if len(eth_addresses) == 0:
@@ -50,17 +36,14 @@ def get_stats_function(fid):
     return_package["hasVerifiedAddress"] = True
     eth_address = eth_addresses[0]
 
-    #TODO check if there is a delegate
-
-    #TODO check if it is a good delegate
-
-    #TODO return delegate info (delgate name or ENS)
-
+    # TODO: Implement delegate checks
+    
     return return_package
 
-
-print(get_stats_function(fid))
-
-
-
-
+def lambda_handler(event, context):
+    fid = event.get('fid', '192336')  # Default to 192336 if no fid provided
+    result = get_stats_function(fid)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(result)
+    }
